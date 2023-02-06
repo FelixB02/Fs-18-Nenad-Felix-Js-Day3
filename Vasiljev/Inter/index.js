c14978f9aebc58d4e1194236eff785641876714b
Nenad Vasiljev <vasiljevnenad1@gmail.com>
2023-02-06 12:55:30 +0100

    inter finished

--- Vasiljev/Inter/index.js
+++ Vasiljev/Inter/index.js
@@ -38,20 +38,25 @@ for(let i = 0; i < 100; i++) {
     }
 }
 
-for(let i = 1; i < 100; i++) {
-    if(i % 3 === 0 && i % 5 == 0) {
-        console.log('fizz buzz')
-    } else if (i % 5 === 0) {
-        console.log('buzz')
-    } else if (i % 3 == 0) {
-        console.log('fizz ')
-    } else {
-        console.log(i)
-    }
+// for(let i = 1; i < 100; i++) {
+//     if(i % 3 === 0 && i % 5 == 0) {
+//         console.log('fizz buzz')
+//     } else if (i % 5 === 0) {
+//         console.log('buzz')
+//     } else if (i % 3 == 0) {
+//         console.log('fizz ')
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+//ex 3 
+
+let star = ''
+
+
+for (let i = 0; i < 8; i++) {
+    star += '*'
+    document.getElementById('stars').innerHTML += `${star} <br>`
 }
-
-
-
-
-
 
